4c55a93dbca8eb64ebe29ffc58af8e4fe33e1909
50002 <142967542+50002@users.noreply.github.com>
2025-01-30 13:58:38 +0200

    ArmBlaster

--- new-game-project/Enemy.gd
+++ new-game-project/Enemy.gd
@@ -6,7 +6,7 @@ var direction
 var Health = 3
 
 func _ready() -> void:
-	player = get_parent().get_node("CharacterBody2D")
+	player = get_parent().get_node("Player")
 	
 	
 func _process(delta: float) -> void:
--- new-game-project/boss.gd
+++ new-game-project/boss.gd
@@ -1,19 +1,35 @@
 extends CharacterBody2D
 
+
+var player 
+const BASE_SPEED = 500
+var direction
+
 var HP := 50
 const Actions = [1, 2, 3]
 var blaster = preload("res://armblaster.tscn")
+
+func _ready() -> void:
+	player = get_parent().get_node("Player")
+
+
 func _physics_process(delta: float) -> void:
-	print(str(Actions[0]))
-	print(str(Actions[1]))
-	print(str(Actions[2]))
+	print(str($"CollisionShape2D/ArmblasterSpawn".global_position))
+	
+	direction=(player.position - $".".position).normalized()
+	velocity = direction * BASE_SPEED * delta 
+	if player.position.x < position.x:
+		$CollisionShape2D.scale = Vector2(-1, 1)
+	else:
+		$CollisionShape2D.scale = Vector2(1, 1)
+	move_and_slide()
 	
 
 func _on_attack_timer_timeout() -> void:
 	var atk = Actions.pick_random()
 	if atk == 1:
 		var shot = blaster.instantiate()
-		shot.pos = $"CollisionShape2D/Attacks/ArmblasterSpawn".global_position
+		shot.pos = $"CollisionShape2D/ArmblasterSpawn".global_position
 		get_parent().add_child(shot)
 	elif atk == 2:
 		pass #laser
